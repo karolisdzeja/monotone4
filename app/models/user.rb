8ceda888366224681dfa81e6b5889acc5c644e8c
Karolis Dzeja <karolis@gmail.com>
2013-07-22 02:44:47 +0000

    Allow users to Sign In using their username or email

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,22 @@ class User < ActiveRecord::Base
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :authentication_keys => [:login]
 
   validates :name, presence: true, uniqueness: { case_sensitive: false }
+  
+  attr_accessible :name, :email, :password, :password_confirmation, :remember_me
+  
+  attr_accessor :login
+  attr_accessible :login
+  
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    else
+      where(conditions).first
+    end
+  end
+  
 end
